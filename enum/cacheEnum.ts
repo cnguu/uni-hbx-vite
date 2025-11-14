d0143fd1989cfe90fc92dfc79d4a894779fb7d2a
cnguu <www@cnguu.cn>
2025-11-14 16:51:01 +0800

    feat: 移除多余的本地缓存

--- enum/cacheEnum.ts
+++ enum/cacheEnum.ts
@@ -9,14 +9,10 @@
 export enum CacheKeyEnum {
   /** 授权令牌 */
   TOKEN = 'uhv_token',
-  /** 最新的 tab 页路径 */
-  LATEST_TAB_URL = 'uhv_latest_tab_url',
 }
 
 /** 存储值的类型映射 */
 export type CacheValueType = {
   /** 授权令牌 */
   [CacheKeyEnum.TOKEN]: string
-  /** 最新的 tab 页路径 */
-  [CacheKeyEnum.LATEST_TAB_URL]: string
 }
--- store/module/useAppStore.ts
+++ store/module/useAppStore.ts
@@ -1,6 +1,3 @@
-import { CacheKeyEnum } from '@/enum/cacheEnum.ts'
-import { uniStorage } from '@/util/storageUtil.ts'
-
 /**
  * 应用仓储
  */
@@ -8,16 +5,13 @@ export const useAppStore = defineStore('app', () => {
   const userStore = useUserStore()
 
   // 最新的 tab 页路径
-  const latestTabUrl = ref<string>(uniStorage.get(CacheKeyEnum.LATEST_TAB_URL) || '')
+  const latestTabUrl = ref<string>('')
   const setLatestTabUrl = (value: string) => {
     latestTabUrl.value = value
-    uniStorage.set(CacheKeyEnum.LATEST_TAB_URL, latestTabUrl.value)
   }
 
   // 重置所有数据
   const reset = () => {
-    setLatestTabUrl('')
-
     userStore.reset()
 
     // #ifdef APP
