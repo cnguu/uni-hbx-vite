55c13b411003b73850947c42b1481d9c0d008a71
cnguu <www@cnguu.cn>
2025-11-14 17:06:52 +0800

    feat: 增加初始化鉴权

--- App.vue
+++ App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { interceptorOptions } from '@/interceptor/route.ts'
+
 onLaunch(() => {
   console.log(`【APP】onLaunch`)
 
   // #ifdef APP
   plus.device.setWakelock(true)
   // #endif
+
+  const currentPage = getCurrentPage()
+  const currentRoute = currentPage ? `/${currentPage.route}` : void 0
+  interceptorOptions.invoke?.({
+    url: currentRoute,
+  })
 })
 
 onShow(() => {
